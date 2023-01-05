a6adab8eace6f57c68a1027f60deb42d6cc311ca
Sinan URUN <sinanurun24@gmail.com>
2023-01-05 16:14:23 +0300

    Create array_cokboyutlu_01.php

---
+++ array_cokboyutlu_01.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$cars = array (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+);
+    
+for ($row = 0; $row < 4; $row++) {
+  echo "<p><b>Row number $row</b></p>";
+  echo "<ul>";
+  for ($col = 0; $col < 3; $col++) {
+    echo "<li>".$cars[$row][$col]."</li>";
+  }
+  echo "</ul>";
+}
+?>
+
+</body>
+</html>
